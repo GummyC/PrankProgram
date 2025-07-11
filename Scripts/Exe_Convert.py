c345f90beafaf4221417b6cf3da83865d026874f
Ethan C <ethanjc@outlook.com>
2025-07-10 22:26:04 -0700

    restructuring project to improve ease of use
    after putting more thought into the project, i decided it would be more efficient for the launcher to load an exe rather than try and set up python

---
+++ Scripts/Exe_Convert.py
@@ -0,0 +1,17 @@
+import subprocess
+from pathlib import Path
+print()
+directory = str(Path(__file__).parent.absolute())
+full_directory = ("cd "+(directory.title()).replace("\\","/"))
+# command = [f"cd {directory}"]
+
+print(full_directory)
+command_list = [full_directory,"dir"]
+
+command = f'{full_directory} && dir && py -m PyInstaller --onefile --clean main_script.py'
+
+
+
+subprocess.run(command, shell=True)
+
+    
