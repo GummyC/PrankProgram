ccf813899f6066e4a019d3fc1b0374cd27b7df31
Ethan C <ethanjc@outlook.com>
2025-07-10 22:51:45 -0700

    updated the launcher to exe

---
+++ launch_files/Exe_Convert.py
@@ -0,0 +1,19 @@
+import subprocess
+from pathlib import Path
+
+
+print()
+directory = str(Path(__file__).parent.absolute())
+full_directory = ("cd "+(directory.title()).replace("\\","/"))
+# command = [f"cd {directory}"]
+
+print(full_directory)
+command_list = [full_directory,"dir"]
+
+command = f'{full_directory} && dir && py -m PyInstaller --onefile --clean loader.py'
+
+
+
+subprocess.run(command, shell=True)
+
+    
